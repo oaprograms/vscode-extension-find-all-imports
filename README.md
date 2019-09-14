a7899d8ba7ae332c71053564ec98aec6e573bfb0
oaprograms <ognjen.apic@gmail.com>
2019-09-15 00:11:00 +0200

    initial

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Find All Imports
 
-VS Code extension that finds all Javascript files that import current file
+VS Code extension that finds all Javascript files that import currently active file
---
+++ extension.js
@@ -0,0 +1,110 @@
+const vscode = require('vscode');
+
+const acornLoose = require("acorn-loose"); // js parser
+const path = require('path');
+const fs = require('fs');
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	let disposable = vscode.commands.registerCommand('extension.findAllImports', function () {
+		// list all files in a folder / skip node_modules etc.
+		const walkSync = (dir, filelist = []) => {
+			fs.readdirSync(dir).forEach(file => {
+				function wantToWalk(file) {
+					return ! ['node_modules', 'bower_components', '.git'].includes(file)
+				}
+				filelist = (fs.statSync(path.join(dir, file)).isDirectory() && wantToWalk(file))
+					? walkSync(path.join(dir, file), filelist)
+					: filelist.concat(path.join(dir, file));
+			});
+			return filelist;
+		}
+		// resolve import path into absolute path
+		function getAbsPath(importPath, startingPath){
+			if (importPath.length) {
+				let path = importPath.split('/');
+				let retPath = startingPath.split('\\');
+				retPath.pop();
+				for (let part of path){
+					if (part.trim()) {
+						if (part === '.') {
+							// do nothing
+						} else if (part === '..') {
+							retPath.pop();
+						} else {
+							retPath.push(part);
+						}
+					}
+				}
+				return retPath.join('\\');
+			} else {
+				return importPath;
+			}
+		}
+
+		function appendDotJs(filename) {
+			return filename.toLowerCase().endsWith('.js') ? filename : filename + '.js';
+		}
+
+		let currentlyOpenTabfilePath = vscode.window.activeTextEditor.document.fileName;
+		let workspaceFolders = vscode.workspace.workspaceFolders;
+		
+		// if at least 1 project folder is open and there is 1 active js file in the editor
+		if (workspaceFolders && workspaceFolders.length && 
+			currentlyOpenTabfilePath && 
+			currentlyOpenTabfilePath.toLowerCase().endsWith('.js')) {
+
+			// find all js files within current projects
+			let workspaceFolderPaths = workspaceFolders.map(folder => folder.uri.fsPath);
+			let jsFiles = [];
+			for (const path of workspaceFolderPaths) {
+				jsFiles = jsFiles.concat(walkSync(path).filter(f => f.toLowerCase().endsWith('.js')))
+			}
+
+			// find all files that import current file
+			let filesThatImportCurrentFile = [];
+			for (const fileName of jsFiles) {
+				let parsed = acornLoose.parse(fs.readFileSync(fileName, 'utf8'));
+				for (const item of parsed.body){
+					if (item.type === "ImportDeclaration") {
+						let importPath = item.source.value;
+						// check if local path
+						if (importPath.startsWith('.')) {
+							// transform it to absolute path
+							let absPath = getAbsPath(importPath, fileName);
+							if (appendDotJs(absPath).toLowerCase() === currentlyOpenTabfilePath.toLowerCase()) {
+								filesThatImportCurrentFile.push(fileName);
+							}
+						}
+					}
+				}
+			}
+			
+			// display file list
+			vscode.window.showQuickPick(filesThatImportCurrentFile.map((fn, index) => ({
+				id: index,
+				label: fn,
+				description: ''
+				//detail: 'aaaaaaaaaaaa'
+			}))).then(item => {
+				vscode.workspace.openTextDocument(item.label).then(document => {
+					vscode.window.showTextDocument(document, {preview: false});
+				});
+			});
+		}
+	});
+
+	context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
